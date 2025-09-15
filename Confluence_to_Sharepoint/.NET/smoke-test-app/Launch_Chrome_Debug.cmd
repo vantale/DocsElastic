bfcca1b95adce08baadd9f13d1fc5f8f94e67c5d
Vantale <vantalo.contact@gmail.com>
2025-09-15 22:59:27 +0200

    Added Working Version of the app

--- Confluence_to_Sharepoint/.NET/smoke-test-app/Launch_Chrome_Debug.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" chrome.exe --remote-debugging-port=9222 --profile-directory=Default
--- Confluence_to_Sharepoint/.NET/smoke-test-app/Launch_Edge_Debug.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" msedge.exe --remote-debugging-port=9222 --profile-directory=Default
